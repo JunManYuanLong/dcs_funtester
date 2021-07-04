3b3418199c50c58ca9c1014819290f52273bf4a5
FunTester <FunTester@88.com>
2021-07-04 21:39:11 +0800

    允许启动无master服务

--- slave/src/main/groovy/com/funtester/slave/common/StartRun.java
+++ slave/src/main/groovy/com/funtester/slave/common/StartRun.java
@@ -1,6 +1,5 @@
 package com.funtester.slave.common;
 
-import com.funtester.base.exception.FailException;
 import com.funtester.slave.manager.SlaveManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +14,7 @@ public class StartRun implements CommandLineRunner {
     @Override
     public void run(String... args) {
         SlaveManager.getIP();
-        boolean register = SlaveManager.register();
-        if (!register) FailException.fail("注册失败!");
+        SlaveManager.register();
         logger.info("程序初始化运行方法执行完毕……");
     }
 
